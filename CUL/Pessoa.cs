a308601d46f6491328a742e3b61e748a6d213125
Wescley Matos <wescleymatos@hotmail.com>
2014-06-25 18:25:04 -0300

    Criando entidade pessoa

--- CUL/Pessoa.cs
+++ CUL/Pessoa.cs
@@ -8,5 +8,25 @@ namespace CUL
 {
     class Pessoa
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Complemento { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string CEP { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string NumeroIdentidade { get; set; }
+        public string OrgaoIdentidade { get; set; }
+        public string EmissorIdentidade { get; set; }
+        public string EstadoIdentidade { get; set; }
+        public string Matricula { get; set; }
+        public string Cargo { get; set; }
+        public string Funcao { get; set; }
+        public string Email { get; set; }
+        public string DescricaoAtividadesDesempenha { get; set; }
     }
 }
